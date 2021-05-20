54913a7d17bea39e6c4259b2a91a22a3d76af348
JOSÉ <survivor1186@gmail.com>
2021-05-20 16:20:12 -0600

    Hoja de Trabajo 01

--- Hoja_de_Trabajo_01.py
+++ Hoja_de_Trabajo_01.py
@@ -1,7 +1,7 @@
 # Python 3: Hoja de trabajo 0
 peso = input ("¿Cuál es su peso en Kg?")
-estatura = input ("¿Cuál es su estaruta en metros?")
+estatura = input ("¿Cuál es su estatura en metros?")
 
-#Para calcular IMC = peso/(estatura*estarura)
+#Para calcular IMC = peso/(estatura*estatura)
 imc= round(float (peso)/float (estatura)**2,2 )
 print(" Su indice de masa corporal es: IMC =",imc)
